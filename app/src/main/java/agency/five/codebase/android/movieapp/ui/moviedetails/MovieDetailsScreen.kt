7ce00cbc9cdd9cdb7b35d23dc0b6b97dfb45aa9e
zecevicmato <mato.zecevic7@gmail.com>
2022-11-17 12:54:35 +0100

    MovieDetailsScreen_wout_MovieDetailsRoute

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/moviedetails/MovieDetailsScreen.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/moviedetails/MovieDetailsScreen.kt
@@ -2,14 +2,21 @@ package agency.five.codebase.android.movieapp.ui.moviedetails
 
 import agency.five.codebase.android.movieapp.R
 import agency.five.codebase.android.movieapp.mock.MoviesMock
+import agency.five.codebase.android.movieapp.ui.component.ActorCard
+import agency.five.codebase.android.movieapp.ui.component.CrewItem
 import agency.five.codebase.android.movieapp.ui.component.FavoriteButton
 import agency.five.codebase.android.movieapp.ui.component.UserScoreProgressBar
 import agency.five.codebase.android.movieapp.ui.moviedetails.mapper.MovieDetailsMapper
 import agency.five.codebase.android.movieapp.ui.moviedetails.mapper.MovieDetailsMapperImpl
+import agency.five.codebase.android.movieapp.ui.theme.MovieAppTheme
 import agency.five.codebase.android.movieapp.ui.theme.spacing
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
@@ -40,6 +48,54 @@ fun MovieDetailsScreen(
         item {
             OverviewSection(movieDetailsViewState)
         }
+        item {
+            CrewmanSection(movieDetailsViewState)
+        }
+        item {
+            CastSection(movieDetailsViewState)
+        }
+    }
+}
+
+@Composable
+fun CastSection(
+    movieDetailsViewState: MovieDetailsViewState,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier.background(MaterialTheme.colors.onPrimary)) {
+        Text(
+            text = stringResource(id = R.string.cast_title),
+            style = MaterialTheme.typography.h5,
+            color = MaterialTheme.colors.primary
+        )
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium),
+        ) {
+            items(movieDetailsViewState.cast.size) { actor ->
+                ActorCard(
+                    actorCardViewState = movieDetailsViewState.cast[actor].actorCardViewState,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CrewmanSection(
+    movieDetailsViewState: MovieDetailsViewState,
+    modifier: Modifier = Modifier,
+) {
+    LazyVerticalGrid(columns = GridCells.Fixed(3),
+        contentPadding = PaddingValues(MaterialTheme.spacing.large),
+        modifier = modifier.padding(start = MaterialTheme.spacing.small)
+    ) {
+        items(
+            movieDetailsViewState.crew
+        ) { crewman ->
+            CrewItem(
+                crewItemViewState = crewman.crewItemViewState,
+            )
+        }
     }
 }
 
@@ -54,7 +110,7 @@ fun OverviewSection(
         Text(
             text = stringResource(id = R.string.overview),
             style = MaterialTheme.typography.h5,
-            color = MaterialTheme.colors.onPrimary,
+            color = MaterialTheme.colors.primary,
             modifier = Modifier.padding(top = MaterialTheme.spacing.medium)
         )
         Text(
@@ -105,5 +161,15 @@ fun ImageSection(
                     .padding(top = MaterialTheme.spacing.medium)
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun MovieDetailsScreenPreview() {
+    MovieAppTheme {
+        MovieDetailsScreen(movieDetailsViewState = movieDetailsViewState,
+            onFavoriteButtonClick = { }
+        )
     }
 }
